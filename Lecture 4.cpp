f3c0bce294ae08398444cda9a89fdb9907970878
Diwakar Sharma <118611994+diwakar7sharma@users.noreply.github.com>
2024-06-17 14:31:12 +0530

    Committing Lecture 4

---
+++ Lecture 4.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a=4,b=5;
+    cout<<"Addition of a and b : "<<a+b<<endl;
+    cout<<"Subtraction of a and b : "<<a-b;
+    return 0;
+}
