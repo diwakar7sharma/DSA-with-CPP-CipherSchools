a0893d691e88ce5765596bec1da1e9a3d7c64d6a
Diwakar Sharma <118611994+diwakar7sharma@users.noreply.github.com>
2024-06-19 16:42:54 +0530

    Committing Lecture 10

---
+++ Lecture 10.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=0;
+    cout<<x<<endl;
+    cout<<++x<<endl;
+    cout<<x<<endl;
+    cout<<x++<<endl;
+
+    for(int i=1;i<101;i++){
+        cout<<i<<" ";
+    }
+    return 0;
+}
