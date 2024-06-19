4854962c1d5c407e5d98c024ac1abead2a7f4e1f
Diwakar Sharma <118611994+diwakar7sharma@users.noreply.github.com>
2024-06-19 13:56:42 +0530

    Committing Lecture 7

---
+++ Lecture 7.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n[5];
+    int a,b,c,d,e;
+    cout<<"Enter 5 Numbers : ";
+    cin>>a>>b>>c>>d>>e;
+    cout<<"Input Order of Numbers : ";
+    cout<<a<<b<<c<<d<<e<<endl;
+    cout<<"Reversed Order : ";
+    cout<<e<<d<<c<<b<<a<<endl;
+    return 0;
+}
